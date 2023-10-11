cff55c0fa02705084a8780a246e2ca9d04c29616
yasmeenbsharat <y.bsharat@student.aaup.edu>
2023-10-11 20:51:03 +0300

    Add profile functions and css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,58 @@
-var current = null;
-document.querySelector('#email').addEventListener('focus', function(e) {
+
+const loginBtn =document.getElementById('login');
+
+
+loginBtn.addEventListener('click', function(){
+    const password=document.getElementById('password').value;
+    const userName =document.getElementById('userName').value;
+    if (userName =='' || password ==''){
+ 
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Your name or password is empty!! Please enter them ^_^',
+      });
+     
+
+    }
+    
+    else {
+       userLogin(userName,password);
+    }
+
+
+});
+async function  userLogin(userName,password){
+    const request = await fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          // username: 'kminchelle',
+          // password: '0lelplR',
+          username: `${userName}`,
+          password: `${password}`,
+        })
+      });
+    const response = await request.json();
+    console.log(response);
+    if(!response.message){
+        localStorage.setItem('token',response.token);
+        window.location.href = '../../pages/profile.html';
+        console.log(response);
+    }
+    else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Your name or password is incorrect!! Please try again ^_^',
+          });
+    }
+  
+   
+ }
+
+let current = null;
+document.querySelector('#userName').addEventListener('focus', function(e) {
   if (current) current.pause();
   current = anime({
     targets: 'path',
@@ -31,7 +84,7 @@ document.querySelector('#password').addEventListener('focus', function(e) {
     }
   });
 });
-document.querySelector('#submit').addEventListener('focus', function(e) {
+document.querySelector('#login').addEventListener('focus', function(e) {
   if (current) current.pause();
   current = anime({
     targets: 'path',
---
+++ assets/js/profile.js
@@ -0,0 +1,124 @@
+let user;
+let profile =document.getElementById("profile");
+function initApp() {
+ Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Logged in Successfully ..!',
+    showConfirmButton: false,
+    timer: 1500
+  });
+  fetchUserProfile();
+}
+ 
+  function fetchUserProfile() {
+    const token = localStorage.getItem('token');
+     user = decodeToken(token);
+   
+    console.log(user.id);
+ generateData (getUserDetails,displayUserDetails);
+}
+
+
+async function  getUserDetails(){
+const request = await fetch(`https://dummyjson.com/users/${user.id}`);
+user = await request.json();
+return user;
+}
+
+function displayUserDetails(user){
+let data='';
+    data +=`<div class="row">
+    <div class="col-md-4">
+        <div class="profile-img">
+            <img src="${user.image}" alt="userImg" class=''/>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="profile-head mt-5">
+                    <h5>
+                    ${user.firstName} ${user.lastName}
+                    </h5>
+                    <h6 class="text-primary">
+                       @ ${user.maidenName}
+                    </h6>
+                    <p class="profile-rating text-uppercase mt-3">Age : <span class='text-primary'> ${user.age}</span> 	&nbsp; 	&nbsp; 	&nbsp;    gender : <span class="text-lowercase text-primary"> ${user.gender}</span></p>
+                    <h5 class="mt-3">About</h5>
+                    <hr>
+        </div>
+    </div>
+    <div class="col-md-2 mt-5">
+        <input type="submit" class="log-out-btn text-primary" name="btnAddMore" value="Log Out" />
+    </div>
+</div>
+<div class="row">
+<div class="col-md-4">           
+</div>
+    <div class="col-md-8">
+        <div class="tab-content profile-tab" id="myTabContent">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>User Id</label>
+                            </div>
+                            <div class="col-md-6">
+                                <p>${user.id}</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>Name</label>
+                            </div>
+                            <div class="col-md-6">
+                                <p>${user.firstName}  ${user.lastName}</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>Email</label>
+                            </div>
+                            <div class="col-md-6">
+                                <p>${user.email}</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>Phone</label>
+                            </div>
+                            <div class="col-md-6">
+                                <p>${user.phone}</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>BirthDate</label>
+                            </div>
+                            <div class="col-md-6">
+                                <p>${user.birthDate}</p>
+                            </div>
+                        </div>
+            </div>
+      
+        </div>
+    </div>
+</div>  `;
+profile.innerHTML = data;}
+
+
+
+function decodeToken(token) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace('-', '+').replace('_', '/');
+    const payload = JSON.parse(atob(base64));
+    return payload;
+}
+
+
+
+
+async function generateData (callback,display){
+    const data=await callback();
+    display(data);
+
+ }
+initApp();
