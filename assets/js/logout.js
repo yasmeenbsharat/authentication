7b1c1c1fa0f37856b22532d8049dfec916c3f3c1
yasmeenbsharat <y.bsharat@student.aaup.edu>
2023-10-23 12:26:26 +0300

    Update logout file

--- assets/js/logout.js
+++ assets/js/logout.js
@@ -4,7 +4,7 @@ function logoutUser() {
     const broadcastChannel = new BroadcastChannel('logoutChannel');
     broadcastChannel.postMessage({ action: 'logout' });
     broadcastChannel.close();
-    window.location.href = ' ';
+    window.location.href = '../';
 }
 
     
